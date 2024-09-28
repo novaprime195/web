0df9829176c36ba6c38822f8d0bb6aaee5392ed2
aunchagaonkar <ameya21unchagaonkar@gmail.com>
2024-09-28 18:28:57 +0530

    Updated

---
+++ WebPanel/app.js
@@ -0,0 +1,16 @@
+///Edit Under This only
+
+
+    var firebaseConfig = {
+    apiKey: "AIzaSyDj0VZf8Ul2B2kk1jPv6PSfJGMDky1IDrk",
+    authDomain: "airavat-34c0a.firebaseapp.com",
+    databaseURL: "https://airavat-34c0a-default-rtdb.firebaseio.com",
+    projectId: "airavat-34c0a",
+    storageBucket: "airavat-34c0a.appspot.com",
+    messagingSenderId: "605171601741",
+    appId: "1:605171601741:web:334d5c0d322cc337e1a704"
+  }; 
+ 
+////Do Not Edit Below This
+
+firebase.initializeApp(firebaseConfig);
